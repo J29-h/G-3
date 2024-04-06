36c21a6a3b1e9648692af302578223c90effd477
J29-h <76265174+J29-h@users.noreply.github.com>
2024-04-06 03:17:21 +0300

    Update ParserFactory.java

--- hangman/ParserFactory.java
+++ hangman/ParserFactory.java
@@ -1,11 +1,21 @@
-
 // implementing the Factory design pattern to this class...  
 // the ParserFactory is the creator class of the factory design
 // pattern. it is responsible for declaring the factory method 
 // that will return the new (same type as product interface) 
-// product obj 
+// product obj
 
-public interface ParserFactory {
+public class ParserFactory {
 
-	  Parser createParser(String filePath);
+	public Parser createParser(String fileType) {
+
+		if (fileType.equals(null)) {
+			return null;
+		}
+
+		if (fileType.equals("./wordlist.txt")) {
+			return new WordParser(fileType);
+		}
+		// When wanting to add new different parser types add more conditions if needed
+		return null;
+	}
 }
