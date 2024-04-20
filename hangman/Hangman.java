43f5eee05d66a13101f0730246cd73d5f78cd9f8
J29-h <76265174+J29-h@users.noreply.github.com>
2024-04-20 20:52:51 +0300

    Update Hangman.java

--- hangman/Hangman.java
+++ hangman/Hangman.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 class Hangman {
 
     public static void main(String[] args) {
-        Printer printer = new Printer();
+        Printer printer = Printer.getInstance();
         printer.printTitle();
 
         WordParser wordparser = new WordParser("./wordlist.txt");
